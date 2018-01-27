615eaf1663323ea72dda25508b04685c008043db
PraveenMax <com.java.praveen@gmail.com>
2018-01-28 01:29:55 +0530

    Changed 'char *' to 'const char *' for passing unallocated char data passing.  Changed the int and float ptrs to normal ones. PrintDetails() implemented thus the child class can blindly call it.

--- CPP_EmpOOPS/Employee.cpp
+++ CPP_EmpOOPS/Employee.cpp
@@ -3,52 +3,61 @@
 #include <iostream>
 using namespace std;
 
-Employee::Employee()
-{
-	
-}
-
-Employee::Employee(char * name, int * empID, char * address, float * salary) {
+ Employee::Employee(const char * name, int  empID, const char * address, float  salary) {
 	this->name = name;
 	this->empID = empID;
 	this->address = address;
 	this->salary = salary;
+
+	cout << "\t  -Employee values are initialized" << endl;
 }
 
-void Employee::SetName(char * t_name)
+void Employee::SetName(const char * t_name)
 {
 	this->name = t_name;
 }
 
-char * Employee::GetName() {
+const char * Employee::GetName() {
 	return this->name;
 }
 
-void Employee::SetSalary(float * t_salary) {
+void Employee::SetSalary(float t_salary) {
 	this->salary = t_salary;
 }
 
-float * Employee::GetSalary() {
+float Employee::GetSalary() {
 	return this->salary;
 }
 
-void Employee::SetEmpID(int * t_empID) {
-	this->empID - t_empID;
+void Employee::SetEmpID(int t_empID) {
+	this->empID = t_empID;
 }
 
-int * Employee::getEmpID() {
+int Employee::getEmpID() {
 	return this->empID;
 }
 
-void Employee::SetAddress(char * t_address) {
+void Employee::SetAddress(const char * t_address) {
 	this->address = t_address;
 }
 
-char * Employee::GetAddress() {
+const char * Employee::GetAddress() {
 	return this->address;
 }
 
 Employee::~Employee() {
-	cout << "Employee object " << this->name << "destroyed." << endl;
+	cout << "Employee object " << this->name << " destroyed." << endl;
 }
+
+
+void Employee::PrintDetails() {
+	cout<< "Details are : " << endl;
+	cout<< "===[FROM EMPLOYEE CLASS]====" << endl;
+	cout << "Name    : "<<this->name << endl;
+	cout << "EmpID   : " << this->empID << endl;
+	cout << "Address : " << this->address << endl;
+	cout << "Salary  : " << this->salary << endl;
+	cout << "-----------------------" << endl;
+}
+
 
