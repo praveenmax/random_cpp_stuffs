4e8325f99d639fa689db71f73bb381dd099f3792
PraveenMax <com.java.praveen@gmail.com>
2018-03-10 01:46:50 +0530

    Implemented Bubble Sort with decorated output.

---
+++ CPP_SortingAlgorithms/CPP_SortingAlgorithms/CPP_SortingAlgorithms/SortingAlgorithm.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include "SortingAlgorithm.h"
+#include "iostream"
+using namespace std;
+
+SortingAlgorithm::SortingAlgorithm()
+{
+
+}
+
+SortingAlgorithm::~SortingAlgorithm()
+{
+}
+
+int * SortingAlgorithm::BubbleSort(int * inputArray, int arraySize)
+{
+	cout << "Sorting using BubbleSort" << endl;
+	bool isSwapped = false;
+
+	for (int i = 0; i < arraySize - 1 ; i++)
+	{
+		isSwapped = false;
+		cout << "Iteration "<<i<<" Starts" << endl<<"-----------------------"<<endl;
+		for (int j = 0; j < (arraySize - 1 - i); j++)
+		{
+			if (inputArray[j] > inputArray[j + 1])
+			{
+				int temp = inputArray[j];
+				inputArray[j] = inputArray[j + 1];
+				inputArray[j + 1] = temp;
+
+				isSwapped = true;
+			}
+
+			//print the array status
+			for (int i = 0; i < arraySize; i++)
+			{
+				cout << inputArray[i] << " | ";
+			}
+			cout << endl;
+		}
+		cout << "Iteration End" << endl << "-----------------------"<<endl;
+
+		if (!isSwapped)
+			break;
+	}
+
+
+	return inputArray;
+}
---
+++ CPP_SortingAlgorithms/CPP_SortingAlgorithms/CPP_SortingAlgorithms/SortingAlgorithm.h
@@ -0,0 +1,9 @@
+#pragma once
+class SortingAlgorithm
+{
+public:
+	SortingAlgorithm();
+	~SortingAlgorithm();
+	int * BubbleSort(int * unsortedArray, int arraySize);
+};
+
